b7f20946bdc60b471f8572a31849ceac4176c3d6
Kostas <kostasp@icloud.com>
2023-12-09 17:59:00 +0100

    add controller file

--- bloglist/index.js
+++ bloglist/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 /*const blogSchema = new mongoose.Schema({
   title: String,
@@ -20,8 +21,9 @@ const mongoUrl = `mongodb+srv://ovvodev:${password}@cluster0.rarbpyh.mongodb.net
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
+/*app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
@@ -38,7 +40,7 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
-
+*/
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
