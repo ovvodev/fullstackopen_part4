2982b03bb33eebdc47150e156804468bc5585bfc
Kostas <kostasp@icloud.com>
2023-12-13 11:05:45 +0100

    4.3 4.4 test implement

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+  
+    const sumOfLikes = (list, value) => {
+      return list.reduce((acc, obj) => acc + obj[value],0 )
+    }
+    
+    return sumOfLikes(blogs, 'likes')
+  }
+
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
   }
