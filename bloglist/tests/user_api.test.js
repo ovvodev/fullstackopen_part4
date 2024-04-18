51cc1e469521eeeb290b2f738b09d808cb52a7b6
Kostas <kostasp@icloud.com>
2024-04-18 19:53:27 +0200

    user bugs
    fix bugs for user creation

--- bloglist/tests/user_api.test.js
+++ bloglist/tests/user_api.test.js
@@ -3,34 +3,31 @@ const User = require('../models/user')
 
 describe('when there is initially one user in db', () => {
 
-    beforeEach(async () => {
-        await User.deleteMany({})
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await usersInDb()
 
-        const passwordHash = await bcrypt.hash('sekret', 10)
-        const user = new User({ username: 'root', passwordHash })
+  })
 
-        await user.save()
-})
+  test('creation fails with proper statuscode and message if username already taken', async () => {
 
-test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = await usersInDb()
 
     const newUser = {
-        username: 'kostas',
-        name: 'Kostas Papathanasiou',
-        password: 'smaragda',
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
     }
 
-    await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+    const usersAtEnd = await usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
 
     const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(newUser.username)
+    expect(usernames).not.toContain(newUser.username)
+  })
 })
-
--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -19,13 +19,21 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  }else if(error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error collection')){
+    return response.status(400).json({ error: 'username must be unique' })
   }
 
   next(error)
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  usersInDb
 }
