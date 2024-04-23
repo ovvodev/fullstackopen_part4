b765c3395f287a55fb74f3e1278000bc56880509
Kostas <kostasp@icloud.com>
2024-04-23 18:04:09 +0200

    login
    implementation of login authentication

--- bloglist/app.js
+++ bloglist/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -25,7 +26,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
--- bloglist/tests/user_api.test.js
+++ bloglist/tests/user_api.test.js
@@ -6,6 +6,24 @@ describe('when there is initially one user in db', () => {
   test('creation succeeds with a fresh username', async () => {
     const usersAtStart = await usersInDb()
 
+    const newUser = {
+      username: 'kostas',
+      name: 'konstantinos papathanasiou',
+      password: 'smaro',
+    }
+
+    await api 
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert.ok(usernames.includes(newUser.username))
+
   })
 
   test('creation fails with proper statuscode and message if username already taken', async () => {
@@ -13,9 +31,9 @@ describe('when there is initially one user in db', () => {
     const usersAtStart = await usersInDb()
 
     const newUser = {
-      username: 'root',
+      username: 'kostas',
       name: 'Superuser',
-      password: 'salainen',
+      password: 'smaro',
     }
 
     const result = await api
