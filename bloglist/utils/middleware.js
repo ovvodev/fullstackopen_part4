06b034bb78b6639c810f7933fa0a2cf6740d8404
Kostas <kostasp@icloud.com>
2024-05-06 18:22:02 +0200

    5.1,5.2
    exercises 5.1.2

--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -1,4 +1,5 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -21,11 +22,21 @@ const tokenExtractor = (request, response, next) => {
 }
 
 const userExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   request.user = await User.findById(decodedToken.id)
+  next()
+  } catch (error) {
+    if (error.name === 'JsonWebTokenError') {
+      return response.status(401).json({
+        error: 'token expired'
+      })
+    }
+    next(error)
+  }
 }
 
 const errorHandler = (error, request, response, next) => {
